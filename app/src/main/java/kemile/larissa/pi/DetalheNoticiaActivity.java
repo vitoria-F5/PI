4f2fe93e63404fd62e17b2a3f7186b51daa0fc21
Josias Neves <jojarbor3@gmail.com>
2022-10-14 10:01:53 -0300

    Login feito e inicio do cadastro e sobre

---
+++ app/src/main/java/kemile/larissa/pi/DetalheNoticiaActivity.java
@@ -0,0 +1,14 @@
+package kemile.larissa.pi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalheNoticiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_noticia);
+    }
+}
